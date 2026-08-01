8169b4292677dc5a8692cd8e042cd7b1179c32d8
JCss156 <136739183+JCss156@users.noreply.github.com>
2026-08-01 13:20:36 -0300

    MPU6050 accelerometer access and reading

--- main.py
+++ main.py
@@ -1,101 +1,119 @@
-potencia = 1023
+x = 1023
 y = 0
-
-graus = 0
+z = 0
+m = 0
 def on_button_pressed_a():
     if True:
         pins.analog_write_pin(AnalogPin.P0, 1023)
-        
+        pins.analog_write_pin(AnalogPin.P1, 1023)
        
     elif False:
         pass
 input.on_button_pressed(Button.A, on_button_pressed_a)
 def button(a: any):
-        global potencia
+        global x
         global y
-        potencia = potencia - 100
-        if potencia < 20:
-            potencia = 1023
+        x = x - 100
+        if x < 20:
+            x = 1023
             y = 0
-        return potencia
+        return x
 def on_button_pressed_b():
     global y
-    global potencia
+    global x
     y = y + 1
-    potencia = button(y)
-    pins.analog_write_pin(AnalogPin.P0, potencia)
-    ##print(potencia)
+    x = button(y)
+    pins.analog_write_pin(AnalogPin.P0, x)
+    ##print(x)
     ##print(y)
-    
+#input.calibrate_compass()
 input.on_button_pressed(Button.B, on_button_pressed_b)
+z = input.acceleration(Dimension.X)
+print(z)
+m = input.compass_heading()
+=
+MPU_ADDRESS = 0x68
+pins.i2c_write_buffer(MPU_ADDRESS, bytes([0x6B, 0x00]))
 
-graus = input.compass_heading()
+def converter_int16(byte_alto, byte_baixo):
+    valor = (byte_alto << 8) | byte_baixo
 
+    if valor >= 32768:
+        valor -= 65536
 
-def forever():
-    aceleracaoX = input.acceleration(Dimension.X)
-    ##print(aceleracaoX)
-    aceleracaoY = input.acceleration(Dimension.Y)
-    ##print(aceleracaoY)
-    aceleracaoZ = input.acceleration(Dimension.Z)
-    print(aceleracaoZ)
+    return valor
 
-
-    anterior = input.compass_heading()
-    
-    basic.pause(50)
-    atual = input.compass_heading()
-        
-    diferenca = atual - anterior
-    if diferenca > 10:
-        print("correcao necessaria")
-
-basic.forever(forever)
-
-def on_forever():
-    graus = input.compass_heading()
-    
-    if graus >= 135 and graus < 225:
-        basic.clear_screen()
-        led.plot(0, 2)
-        led.plot(1, 2)
-        led.plot(2, 2)
-        led.plot(3, 2)
-        led.plot(4, 2)
-        led.plot(0, 1)
-        led.plot(0, 3)
-    elif (graus < 45 and graus >= 0) or (graus <= 360 and graus > 315) :
-        basic.clear_screen()
-        led.plot(0, 2)
-        led.plot(1, 2)
-        led.plot(2, 2)
-        led.plot(3, 2)
-        led.plot(4, 2)
-        led.plot(4, 1)
-        led.plot(4, 3)
-    elif graus > 225 and graus < 315:
-        basic.clear_screen()
-        led.plot(2, 0)
-        led.plot(2, 1)
-        led.plot(2, 2)
-        led.plot(2, 3)
-        led.plot(2, 4)
-        led.plot(1, 4)
-        led.plot(3, 4)
-    elif graus < 135 and graus > 45:
-        basic.clear_screen()
-        led.plot(2, 0)
-        led.plot(2, 1)
-        led.plot(2, 2)
-        led.plot(2, 3)
-        led.plot(2, 4)
-        led.plot(1, 0)
-        led.plot(3, 0)
-    
-    ##print(graus)
-basic.forever(on_forever)
+while True:
+    pins.i2c_write_number(MPU_ADDRESS, 0x3B, NumberFormat.UINT8_BE, True)
+    dados = pins.i2c_read_buffer(MPU_ADDRESS, 14)
+    print(dados)
+    ax_bruto = converter_int16(dados[0], dados[1])
+    ay_bruto = converter_int16(dados[2], dados[3])
+    az_bruto = converter_int16(dados[4], dados[5])
 
 
-print(graus)
+    gx_bruto = converter_int16(dados[8], dados[9])
+    gy_bruto = converter_int16(dados[10], dados[11])
+    gz_bruto = converter_int16(dados[12], dados[13])
+
+    # Configuração padrão do MPU6050:
+    # acelerômetro: ±2 g
+    # giroscópio: ±250 graus/s
+    ax = ax_bruto / 16384
+    ay = ay_bruto / 16384
+    az = az_bruto / 16384
+
+    gx = gx_bruto / 131
+    gy = gy_bruto / 131
+    gz = gz_bruto / 131
+
+    #print(ax)
+    #print(ay)
+    print(az)
+
+
+
+#def on_forever():
+    #    m = input.compass_heading()
+    #    if m >= 135 and m < 225:
+        #        basic.clear_screen()
+        #        led.plot(0, 2)
+        #        led.plot(1, 2)
+        #        led.plot(2, 2)
+        #        led.plot(3, 2)
+        #        led.plot(4, 2)
+        #        led.plot(0, 1)
+        #        led.plot(0, 3)
+        #print(m)
+        #    elif (m < 45 and m > 0) or (m < 360 and m > 315) :
+            #        basic.clear_screen()
+            #       led.plot(0, 2)
+            #       led.plot(1, 2)
+            #       led.plot(2, 2)
+            ##        led.plot(3, 2)
+            #        led.plot(4, 2)
+            #        led.plot(4, 1)
+            #        led.plot(4, 3)
+            #       #print(m)
+            #    elif m > 225 and m < 315:
+                #        basic.clear_screen()
+                #        led.plot(2, 0)
+                #        led.plot(2, 1)
+                #        led.plot(2, 2)
+                #        led.plot(2, 3)
+                #        led.plot(2, 4)
+                #        led.plot(1, 4)
+                #        led.plot(3, 4)
+        #print(m)
+        #    elif m < 135 and m > 45:
+            #       basic.clear_screen()
+            #        led.plot(2, 0)
+            #        led.plot(2, 1)
+            #       led.plot(2, 2)
+            #        led.plot(2, 3)
+            #       led.plot(2, 4)
+            #        led.plot(1, 0)
+            #       led.plot(3, 0)
+        #print(m)
+        #basic.forever(on_forever)
 D = 0
-
--- main.ts
+++ main.ts
@@ -1,9 +1,24 @@
-let potencia = 1023
+let dados: Buffer;
+let ax_bruto: number;
+let ay_bruto: number;
+let az_bruto: number;
+let gx_bruto: number;
+let gy_bruto: number;
+let gz_bruto: number;
+let ax: number;
+let ay: number;
+let az: number;
+let gx: number;
+let gy: number;
+let gz: number;
+let x = 1023
 let y = 0
-let graus = 0
+let z = 0
+let m = 0
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     if (true) {
         pins.analogWritePin(AnalogPin.P0, 1023)
+        pins.analogWritePin(AnalogPin.P1, 1023)
     } else if (false) {
         
     }
@@ -12,82 +27,104 @@ input.onButtonPressed(Button.A, function on_button_pressed_a() {
 function button(a: any): number {
     
     
-    potencia = potencia - 100
-    if (potencia < 20) {
-        potencia = 1023
+    x = x - 100
+    if (x < 20) {
+        x = 1023
         y = 0
     }
     
-    return potencia
+    return x
 }
 
-// #print(potencia)
+// #print(x)
 // #print(y)
+// input.calibrate_compass()
 input.onButtonPressed(Button.B, function on_button_pressed_b() {
     
     
     y = y + 1
-    potencia = button(y)
-    pins.analogWritePin(AnalogPin.P0, potencia)
+    x = button(y)
+    pins.analogWritePin(AnalogPin.P0, x)
 })
-graus = input.compassHeading()
-basic.forever(function forever() {
-    let aceleracaoX = input.acceleration(Dimension.X)
-    // #print(aceleracaoX)
-    let aceleracaoY = input.acceleration(Dimension.Y)
-    // #print(aceleracaoY)
-    let aceleracaoZ = input.acceleration(Dimension.Z)
-    console.log(aceleracaoZ)
-    let anterior = input.compassHeading()
-    basic.pause(50)
-    let atual = input.compassHeading()
-    let diferenca = atual - anterior
-    if (diferenca > 10) {
-        console.log("correcao necessaria")
+z = input.acceleration(Dimension.X)
+console.log(z)
+m = input.compassHeading()
+let [] = []
+let MPU_ADDRESS = 0x68
+pins.i2cWriteBuffer(MPU_ADDRESS, pins.createBufferFromArray([0x6B, 0x00]))
+function converter_int16(byte_alto: number, byte_baixo: number): number {
+    let valor = byte_alto << 8 | byte_baixo
+    if (valor >= 32768) {
+        valor -= 65536
     }
     
-})
-// #print(graus)
-basic.forever(function on_forever() {
-    let graus = input.compassHeading()
-    if (graus >= 135 && graus < 225) {
-        basic.clearScreen()
-        led.plot(0, 2)
-        led.plot(1, 2)
-        led.plot(2, 2)
-        led.plot(3, 2)
-        led.plot(4, 2)
-        led.plot(0, 1)
-        led.plot(0, 3)
-    } else if (graus < 45 && graus >= 0 || graus <= 360 && graus > 315) {
-        basic.clearScreen()
-        led.plot(0, 2)
-        led.plot(1, 2)
-        led.plot(2, 2)
-        led.plot(3, 2)
-        led.plot(4, 2)
-        led.plot(4, 1)
-        led.plot(4, 3)
-    } else if (graus > 225 && graus < 315) {
-        basic.clearScreen()
-        led.plot(2, 0)
-        led.plot(2, 1)
-        led.plot(2, 2)
-        led.plot(2, 3)
-        led.plot(2, 4)
-        led.plot(1, 4)
-        led.plot(3, 4)
-    } else if (graus < 135 && graus > 45) {
-        basic.clearScreen()
-        led.plot(2, 0)
-        led.plot(2, 1)
-        led.plot(2, 2)
-        led.plot(2, 3)
-        led.plot(2, 4)
-        led.plot(1, 0)
-        led.plot(3, 0)
-    }
-    
-})
-console.log(graus)
+    return valor
+}
+
+while (true) {
+    pins.i2cWriteNumber(MPU_ADDRESS, 0x3B, NumberFormat.UInt8BE, true)
+    dados = pins.i2cReadBuffer(MPU_ADDRESS, 14)
+    console.log(dados)
+    ax_bruto = converter_int16(dados[0], dados[1])
+    ay_bruto = converter_int16(dados[2], dados[3])
+    az_bruto = converter_int16(dados[4], dados[5])
+    gx_bruto = converter_int16(dados[8], dados[9])
+    gy_bruto = converter_int16(dados[10], dados[11])
+    gz_bruto = converter_int16(dados[12], dados[13])
+    //  Configuração padrão do MPU6050:
+    //  acelerômetro: ±2 g
+    //  giroscópio: ±250 graus/s
+    ax = ax_bruto / 16384
+    ay = ay_bruto / 16384
+    az = az_bruto / 16384
+    gx = gx_bruto / 131
+    gy = gy_bruto / 131
+    gz = gz_bruto / 131
+    // print(ax)
+    // print(ay)
+    console.log(az)
+}
+// def on_forever():
+//     m = input.compass_heading()
+//     if m >= 135 and m < 225:
+//         basic.clear_screen()
+//         led.plot(0, 2)
+//         led.plot(1, 2)
+//         led.plot(2, 2)
+//         led.plot(3, 2)
+//         led.plot(4, 2)
+//         led.plot(0, 1)
+//         led.plot(0, 3)
+// print(m)
+//     elif (m < 45 and m > 0) or (m < 360 and m > 315) :
+//         basic.clear_screen()
+//        led.plot(0, 2)
+//        led.plot(1, 2)
+//        led.plot(2, 2)
+// #        led.plot(3, 2)
+//         led.plot(4, 2)
+//         led.plot(4, 1)
+//         led.plot(4, 3)
+//        #print(m)
+//     elif m > 225 and m < 315:
+//         basic.clear_screen()
+//         led.plot(2, 0)
+//         led.plot(2, 1)
+//         led.plot(2, 2)
+//         led.plot(2, 3)
+//         led.plot(2, 4)
+//         led.plot(1, 4)
+//         led.plot(3, 4)
+// print(m)
+//     elif m < 135 and m > 45:
+//        basic.clear_screen()
+//         led.plot(2, 0)
+//         led.plot(2, 1)
+//        led.plot(2, 2)
+//         led.plot(2, 3)
+//        led.plot(2, 4)
+//         led.plot(1, 0)
+//        led.plot(3, 0)
+// print(m)
+// basic.forever(on_forever)
 let D = 0
